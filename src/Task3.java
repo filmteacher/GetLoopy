af1fc2c2385145ba2b9584a34ada4a528fb10e5a
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-06-18 21:48:18 -0400

    Initial commit

---
+++ src/Task3.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args)
+    {
+        for (int i = 30; i >= 1; i--)
+        {
+            System.out.print(" " + i + " ");
+        }
+    }
+}
---
+++ src/Task6.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    public static void main(String[] args)
+    {
+        int stars = 1;
+        for (int row = 1; row <= 5; row++)
+        {
+            for (int col = 1; col <= stars; col++)
+            {
+                System.out.print("* ");
+            }
+            stars++;
+            System.out.println();
+        }
+    }
+}
